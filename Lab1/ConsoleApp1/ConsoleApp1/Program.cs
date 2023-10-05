aa7ea31b509ed00640346a006ab8990a82d1b1ee
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2023-10-05 20:10:08 +0300

    s

---
+++ Lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace lab1
+{
+    public class App
+    {
+        private class Pets
+        {   //props
+            protected string name;
+            protected int age;
+            protected List<string> toys;
+            //Constr
+            public Pets(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+
+            //methods
+            public string Name { get { return name; } }
+            public int Age { get { return age; } }
+
+            public virtual string Does()
+            {
+                return "mizerie";
+            }
+
+
+        }
+
+        private class Cat : Pets
+        {
+            public Cat(string name, int age) : base(name, age)
+            {
+            }
+
+            public string Does()
+            {
+                return "miau";
+            }
+
+        }
+        private class Dog : Pets
+        {
+            public Dog(string name, int age) : base(name, age)
+            {
+            }
+
+            public string Does()
+            {
+                return "Ham";
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Azorel", 3);
+
+            Console.WriteLine($"The dog named {myDog.Name}, aged {myDog.Age}, says {myDog.Does()}!");
+        }
+
+
+    }
+}
