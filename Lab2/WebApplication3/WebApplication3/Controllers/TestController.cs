a02021c821795e41cb32a983bc3aa96acf50ac17
Caracaș Radu Nicolae <caracasradu11@gmail.com>
2023-10-12 21:19:41 +0300

    lab2

---
+++ Lab2/WebApplication3/WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public static List<Animal> Animale = new List<Animal>()
+        {
+            new Animal{ Id=1, Name="Azorel" , Description="Azorel cu parul cret"},
+            new Animal{ Id=2, Name="Mieunel" , Description="Mieunel cu parul cret"},
+            new Animal{ Id=3, Name="PUPUNEL" , Description="PUPUNEL cu parul cret"},
+            new Animal{ Id=4, Name="Miorlau" , Description="Miorlau cu parul cret"},
+
+        };
+        [HttpGet]
+        public List<Animal> GetAnimale()
+        {
+            return Animale;
+        }
+    }
+}
---
+++ Lab2/WebApplication3/WebApplication3/Models/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication3.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Animal() { }
+
+    }
+}
